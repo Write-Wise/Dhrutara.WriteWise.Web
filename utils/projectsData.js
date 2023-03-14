1f0aa1fa9f074fa81ec061ed8e5c21eb996bf4d3
keerthiusha <keerthiushakrishnan@outlook.com>
2023-03-13 21:18:59 -0400

    Remove linting

--- utils/projectsData.js
+++ utils/projectsData.js
@@ -1,7 +1,7 @@
 const projects = [
-  { id: 'react', name: 'Privacy Policy', path: 'facebook/react', slug: 'privacy-policy1'},
+  /* { id: 'react', name: 'Privacy Policy', path: 'facebook/react', slug: 'privacy-policy1'},
   { id: 'vue', name: 'Terms of Use', path: 'vuejs/vue', slug: 'terms-of-use'},
-  { id: 'svelte', name: 'User data deletion', path: 'zeit/next.js', slug: 'user-data-deletion' },
+  { id: 'svelte', name: 'User data deletion', path: 'zeit/next.js', slug: 'user-data-deletion' }, */
 ];
 
 module.exports = { projects };
