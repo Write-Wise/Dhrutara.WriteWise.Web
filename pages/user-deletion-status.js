a0285b18e32772727ae4e68237cc90b62be3dc4e
keerthiusha <keerthiushakrishnan@outlook.com>
2023-03-19 12:47:08 -0400

    fix vulnerabilities and make status page more styles.

--- pages/user-deletion-status.js
+++ pages/user-deletion-status.js
@@ -1,18 +1,17 @@
 import useSWR from 'swr'
 import { useRouter } from 'next/router';
  
-function Profile() {
+function UserDeletionStatus() {
   
   const fetcher = (...args) => fetch(...args).then(res => res.text())
   const router = useRouter();
-  let userid  = router.query['userid'] || '';
-  let provider  = router.query['provider'] || '';
-  
+  const userid  = router.query['userid'] || '';
+  const provider  = router.query['provider'] || '';
   const { data, error, isLoading } = useSWR(`/api/GetDeletedUserStatus?userId=${userid}&provider=${provider}`, fetcher)
  
-  if (error) return <div>{error}</div>
-  if (isLoading) return <div>loading...</div>
-  return <div>{data}</div>
+  if (error) return <div className='home'><h1>Sorry, failed to fetch status. Please try again later. We have been informed about this issue.</h1></div>
+  if (isLoading) return <div>Checking status...</div>
+  return <div className='home'><h1>{data}</h1></div>
 }
 
-export default Profile
+export default UserDeletionStatus
